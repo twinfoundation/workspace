f120fc8df0dc01eadebb41238d3fbcbc4322f173
Martyn Janes <martyn@obany.com>
2025-12-04 12:47:07 +0800

    feat: update repo label script

--- scripts/add-repo-labels.mjs
+++ scripts/add-repo-labels.mjs
@@ -1,48 +1,55 @@
-import { Octokit }  from "@octokit/rest";
+import { Octokit } from "@octokit/rest";
 
 const octokit = new Octokit({
-  auth: "github_pat"
+	auth: "github_pat_......"
 });
 
 const ORG = "twinfoundation";
-const LABEL = {
-  name: "information-needed",
-  color: "7ecd6e",
-  description: "More information is needed from the issue reporter",
-};
+const labels = [{
+	name: "needs-triage",
+	color: "f0650b",
+	description: "Issues that need triage",
+},
+{
+	name: "information-needed",
+	color: "7ecd6e",
+	description: "More information is needed from the issue reporter",
+}];
 
 async function addLabelToAllRepos() {
-  try {
-    const repos = await octokit.paginate(octokit.repos.listForOrg, {
-      org: ORG,
-      type: "all",
-      per_page: 100,
-    });
+	try {
+		const repos = await octokit.paginate(octokit.repos.listForOrg, {
+			org: ORG,
+			type: "all",
+			per_page: 100,
+		});
 
-	// To avoid hitting rate limits
-	await new Promise(resolve => setTimeout(resolve, 1000));
-    for (const repo of repos) {
-      try {
-        await octokit.issues.createLabel({
-          owner: ORG,
-          repo: repo.name,
-          ...LABEL,
-        });
-        console.log(`✅ Label added to ${repo.name}`);
-      } catch (err) {
-        if (err.status === 422) {
-          console.log(`⚠️ Label already exists in ${repo.name}`);
-        } else {
-          console.error(`❌ Error in ${repo.name}:`, err.message);
-        }
-      }
+		// To avoid hitting rate limits
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		for (const repo of repos) {
+			for (const label of labels) {
+				try {
+					await octokit.issues.createLabel({
+						owner: ORG,
+						repo: repo.name,
+						...label,
+					});
+					console.log(`✅ Label added to ${repo.name}`);
+				} catch (err) {
+					if (err.status === 422) {
+						console.log(`⚠️ Label already exists in ${repo.name}`);
+					} else {
+						console.error(`❌ Error in ${repo.name}:`, err.message);
+					}
+				}
+			}
 
-	  // To avoid hitting rate limits
-	  await new Promise(resolve => setTimeout(resolve, 1000));
-    }
-  } catch (err) {
-    console.error("Failed to fetch repositories:", err.message);
-  }
+			// To avoid hitting rate limits
+			await new Promise(resolve => setTimeout(resolve, 1000));
+		}
+	} catch (err) {
+		console.error("Failed to fetch repositories:", err.message);
+	}
 }
 
 addLabelToAllRepos();
